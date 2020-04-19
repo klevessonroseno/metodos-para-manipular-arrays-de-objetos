ea94228b536cd09597fa1db1c87a5715c624bc1d
klevessonroseno <klevessonroseno42@gmail.com>
2020-04-19 13:50:16 -0300

    Criando array de objetos

---
+++ index.js
@@ -0,0 +1,22 @@
+const alunos = [
+    {
+        nome: 'Paulo Vilela',
+        media: 5.25
+    },
+    {
+        nome: 'Maria Cláudia',
+        media: 6.9
+    },
+    {
+        nome: 'Roberto Augusto',
+        media: 2.8
+    },
+    {
+        nome: 'Carla Maria',
+        media: 8.25
+    },
+    {
+        nome: 'André Silva',
+        media: 8.2
+    }
+];
